a3708526cb1a9e7da7eb7b23305e27d2858cc096
Lien Tran <lehonglien.tran@gmail.com>
2016-04-01 03:13:13 -0400

    turns processed novel to dataframe of function words

--- data/PROCESSED/novel_to_func_lem_df.py
+++ data/PROCESSED/novel_to_func_lem_df.py
@@ -16,3 +16,47 @@ def novel_to_func_df(filename):
 	func_file = open("function+words.txt", 'r')
 	fws = openfile.read().lower().split()
 	fws = [word for word in fws if '\'' not in word]
+
+	nlp = English()
+	openfile = open(filename, 'r+')
+	s = openfile.read()
+	u = unicode(s, "utf-8")
+
+	# processed unicode string
+	pu = nlp(u)
+
+	# creating a dictionary for effective counting
+	d = {key: [] for key in fws}
+
+	ten_sents = [] # contains chunks of 10 sentences or less
+	chunk = []
+	# for every sentence in the text
+	for i, sent in enumerate(pu.sents):
+		# if it's the 10th, 20th, etc, then start new chunk
+		# else just add it to the current chunk
+		if i%10 == 0:
+			chunk = []
+		chunk.append(sent)
+		# if it's the last sentence of a chunk, then add chunk to ten_sents
+		if i%10 == 9:
+			ten_sents.append(chunk)
+	# edge case: when sentences run out, add whatever chunk is left
+	if chunk:
+		ten_sents.append(chunk)
+
+	for chunk in ten_sents:
+		# turning each chunk into a pure text chunk again
+		text_chunk = [unicode(str(sen), "utf-8") for sen in chunk]
+		text_chunk = ''.join(text_chunk)
+		# process sentence again
+		ps = nlp(text_chunk)
+		for key in d:
+			d[key].append(0)
+		for token in ps:
+			if token.lemma_ in fws:
+				d[token.lemma_][-1] += 1
+
+	df = pd.DataFrame(d)
+	return df
+
+
